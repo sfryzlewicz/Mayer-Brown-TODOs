4e79fc6a4f994cfbd6dcc4c15d86022d3ee117f1
Sara Fryzlewicz <sfryzlewicz@hawk.iit.edu>
2024-01-02 14:03:28 -0600

    Searchbar UI and basic filtering functionality

--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,5 +1,14 @@
-import React, {useState} from "react";
-import {View, Text, StyleSheet, StatusBar, Platform, TextInput, TouchableOpacity, FlatList} from "react-native";
+import React, {useState, useEffect} from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    StatusBar,
+    Platform,
+    TextInput,
+    TouchableOpacity,
+    FlatList,
+  } from "react-native";
 import { Card } from "../components";
 import { COLORS, SIZES, FONTS, SHADOW } from "../constants";
 
@@ -41,16 +50,29 @@ const styles = StyleSheet.create ({
             alignItems: "center",
             justifyContent: "center",
         },
+        searchBar: {
+            ...SHADOW,
+            borderRadius: SIZES.borederRadius,
+            backgroundColor: COLORS.secondary,
+            height: 54,
+            paddingLeft: 15,
+            color: COLORS.primary,
+            width: "100%",
+            ...FONTS.h2_semiBold,
+            marginBottom: 10,
+        },
 })
 
 export default function Homepage(){
     const [list, setList] = useState([])
-    const [userInput, setUserInput] = useState("")
+    const [userToDoInput, setUserToDoInput] = useState("")
+    const [userSearchInput, setUserSearchInput] = useState("")
+    const [filteredTasks, setFilteredTasks] = useState([]);
 
     // Function to add a task
     function addToDo(text) {
         setList((prev) => [...prev, text]);
-        setUserInput("");
+        setUserToDoInput("");
     }
 
     // Function to delete a task
@@ -67,25 +89,47 @@ export default function Homepage(){
         );
     }
 
+    // Function to filter tasks based on search input
+    useEffect(() => {
+        const filtered = list.filter(
+          (task) =>
+            task.text &&
+            (task.text.toLowerCase().includes(userSearchInput.toLowerCase()) ||
+              task.isChecked)
+        );
+        setFilteredTasks(filtered);
+      }, [userSearchInput, list]);
+
     return(
         <>
         <View style={styles.container}>
             <Text style={{...FONTS.h1_semiBold, paddingBottom: 15, color:COLORS.accent, fontSize:30}}> Your To-Do List:</Text>
+            {/* No recorded to-do tasks */}
             {(list.length==0) && <Text style={{...FONTS.h2_semiBold, color:COLORS.secondary, marginLeft:20}}>Please add a task you wish to complete below.</Text>}
+            
+            {/* To-do tasks exist*/}
             {(list.length>=1) && 
-                <FlatList
-                style={{ flex: 1 }}
-                data={list}
-                renderItem={({ item }) => (
-                    <Card text={item} isChecked={item.isChecked} onDelete={deleteToDo} onToggle={toggleCheckbox}/>
-                )}
-                keyExtractor={(item, index) => index.toString()}/>
+                <>
+                    <TextInput
+                    style={{...styles.searchBar, color:COLORS.shadow, height: 40}}
+                    placeholder="Search tasks..."
+                    onChangeText={(text) => setUserSearchInput(text)}
+                    value={userSearchInput}
+                    />
+                    <FlatList
+                    style={{ flex: 1 }}
+                    data={list}
+                    renderItem={({ item }) => (
+                        <Card text={item} isChecked={item.isChecked} onDelete={deleteToDo} onToggle={toggleCheckbox}/>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}/>
+                </>
             }
             </View>
 
             <View style = {styles.textBoxWrapper}>
-                <TextInput style={styles.textInput} placeholder="New Task" onChangeText={text => setUserInput(text)} value={userInput}/>
-                <TouchableOpacity style={styles.button} disabled={userInput==""} onPress={()=>addToDo(userInput)}>
+                <TextInput style={styles.textInput} placeholder="New Task" onChangeText={text => setUserToDoInput(text)} value={userToDoInput}/>
+                <TouchableOpacity style={styles.button} disabled={userToDoInput==""} onPress={()=>addToDo(userToDoInput)}>
                     <Text style={{...FONTS.h1_semiBold, color: COLORS.shadow, fontSize: 30,}}>+</Text>
                 </TouchableOpacity>
             </View>
